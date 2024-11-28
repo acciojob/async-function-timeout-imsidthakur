c43602d2d788305d50a8722f786c728306dd376d
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-11-28 07:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+
+const form = document.getElementsByTagName("form")[0];
+const output = document.getElementById("output");
+
+
+document.getElementById("btn").addEventListener("click",()=>{
+   
+  const val =  form.text.value;
+  const del =  Number(form.delay.value);
+  
+  setTimeout(function() {
+    output.textContent = val + " - " + del;
+  }, del);
+})
+
